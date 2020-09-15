34c91c7b27d2480f046ad5411ed2fed918e2d698
Lazaro <lquin008@gmail.com>
2020-09-15 15:57:21 -0400

    incorporated optimisitic ui update apollo

--- src/context/VoteContext.jsx
+++ src/context/VoteContext.jsx
@@ -7,7 +7,20 @@ const VoteContext = createContext();
 const VoteContextProvider = ({ children }) => {
   const [voter, setVoter] = useState(localStorage.getItem("voterId"))
   const voteData = useQuery(VOTE_QUERY, { client });
-  const [recordVote] = useMutation(VOTE_MUTATION, { client });
+  const [recordVote] = useMutation(VOTE_MUTATION, {
+    client,
+    update(cache, mutationResult) {
+      const { votes } = cache.readQuery({ query: VOTE_QUERY });
+      const newVote = mutationResult.data.recordVote;
+      const updatedVotes = votes.filter(
+        (vote) => newVote.voterId !== vote.voterId
+      );
+      cache.writeQuery({
+        query: VOTE_QUERY,
+        data: { votes: [...updatedVotes, newVote] },
+      });
+    },
+  });
 
   useEffect(()=>{
     if (!localStorage.getItem("voterId")) {
@@ -22,7 +35,14 @@ const VoteContextProvider = ({ children }) => {
         donut,
         voter,
       },
-      refetchQueries: [{ query: VOTE_QUERY }],
+      optimisticResponse: {
+        __typename: "Mutation",
+        recordVote: {
+          __typename: "Vote",
+          voter,
+          donut
+        }
+      }
     });
   };
 
