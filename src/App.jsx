4f8ccc0205d8c35c64af6d23768055e822f98e23
Lazaro <lquin008@gmail.com>
2020-09-14 20:40:17 -0400

    setup App for React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
+import client from "./data/Sanity";
+import { SanityContextProvider, VoteContextProvider } from "./context";
+import { Header, DonutMain } from "./components";
+import GlobalStyles from "./styles/GlobalStyles";
 
-const App = () => {
-  <ApolloProvider>
-    <h1>Hello World of Donuts</h1>
-  </ApolloProvider>;
-};
+const App = () => (
+  <ApolloProvider client={client}>
+    <SanityContextProvider>
+      <VoteContextProvider>
+        <Header />
+        <DonutMain />
+        <GlobalStyles />
+      </VoteContextProvider>
+    </SanityContextProvider>
+  </ApolloProvider>
+);
 
 ReactDOM.render(<App />, document.querySelector("#root"));
