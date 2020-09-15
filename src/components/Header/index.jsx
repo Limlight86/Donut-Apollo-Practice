4deed0fb224ee6e69d6b700ac74f126f816b51bc
Lazaro <lquin008@gmail.com>
2020-09-14 20:41:36 -0400

    Header component added

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeaderWrapper } from "./styled";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h1>Donut of the Day</h1>
+      <h4>Vote for your favorite!</h4>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  position: fixed;
+  background-color: rgb(172, 12, 38);
+  text-align: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+`;
