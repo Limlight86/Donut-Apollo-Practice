e041fef05567ac7feb1b68e036e01e39f60c8f1b
Lazaro <lquin008@gmail.com>
2020-09-14 20:46:09 -0400

    file folder export routing

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as DonutMain } from "./DonutMain";
+export { default as Header } from "./Header";
+export { default as SingleDonut } from "./SingleDonut";
---
+++ src/context/index.js
@@ -0,0 +1,2 @@
+export { SanityContext, SanityContextProvider } from "./SanityContext.jsx";
+export { VoteContext, VoteContextProvider } from "./VoteContext.jsx";
